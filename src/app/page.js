c76a8727d9ed9999a8ee28728fae85d910b51861
Hyzyr <avezberdiyew@gmail.com>
2024-12-04 00:57:21 +0500

    Preloader

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@
 import Preloader from '@/views/preloader/PreloaderScreen';
 import Dynasty from '@/views/scenes/Dynasty';
 import Egypt from '@/views/scenes/Egypt';
+import Prehystoric from '@/views/scenes/Prehystoric';
 
 export default function Home() {
-  return <Egypt  />;
+  return <Preloader  />;
 }
