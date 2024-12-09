d424b6a9bde719c5861b9ec17aa45808f7b22a3a
Hyzyr <avezberdiyew@gmail.com>
2024-12-10 00:07:13 +0500

    cmre fx

--- src/views/scenes/hooks/useCameraAnimations.jsx
+++ src/views/scenes/hooks/useCameraAnimations.jsx
@@ -4,7 +4,7 @@ import gsap from 'gsap';
 import { debounce, throttle } from '@/utils/debounce';
 import { calcMouseFromCenter } from '@/utils/calcEvent';
 
-const bgMove = 8;
+const bgMove = 10;
 const moveFactorMain = 2;
 const moveFactor = 0.7;
 const direction = 1;
