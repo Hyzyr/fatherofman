be48b3a842cd8accfa59c91e0ff42fd85e74b3b7
Hyzyr <avezberdiyew@gmail.com>
2024-12-09 22:06:49 +0500

    FIX PAGE JS

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ import { AllAssetFiles } from '@/contants/Assets';
 import useAssetLoader from '@/hooks/useAssetLoader';
 import Preloader from '@/views/preloader/PreloaderScreen';
 import AScenes from '@/views/scenes/AScenes';
-import Dynasty from '@/views/scenes/dynasty/Dynasty';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
@@ -18,7 +17,6 @@ export default function Home() {
   }, []);
   if (!ready) return null;
 
-  return <Dynasty animated />;
   return (
     <>
       {loaded && <AScenes active={granted} />}
